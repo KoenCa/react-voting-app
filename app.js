172cdbee7784c6d9e261cef7f7a79258c07aa8ff
Koen Castermans <castermanskoen@msn.com>
2019-07-15 21:17:16 +0200

    Add the first reactcomponent

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-undef */
 
+const ProductList = React.createClass({
+  render: function() {
+    return (
+      <div className="ui items">
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<ProductList />, document.getElementById("content"));
