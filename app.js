9d5fd6775cf3b4c1403a307fa55ea1ea4eafa3ff
Koen Castermans <castermanskoen@msn.com>
2019-07-16 21:48:08 +0200

    Render products from data using new component

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 /* eslint-disable no-undef */
 
-const ProductList = React.createClass({
+const Product = React.createClass({
   render: function() {
     return (
-      <div className="ui items">
-        Hello, friend! I am a basic React component.
+      <div className="item">
+        <div className="image">
+          <img src={this.props.product_image_url} />
+        </div>
+        <div className="middle aligned content">
+          <div className="header">
+            <a>
+              <i className="large caret up icon" />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img
+              className="ui avatar image"
+              src={this.props.submitter_avatar_url}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 });
 
+const ProductList = React.createClass({
+  render: function() {
+    const products = Data.map(product => {
+      return (
+        <Product
+          key={'product-' + product.id}
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitter_avatar_url={product.submitter_avatar_url}
+          product_image_url={product.product_image_url}
+        />
+      );
+    });
+    return <div className="ui items">{products}</div>;
+  }
+});
+
+// Apply our component to the DOM and render it
 ReactDOM.render(<ProductList />, document.getElementById("content"));
